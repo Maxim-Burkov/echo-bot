89b00655996bf68ba711227eefce3b8edab1ee86
like_johnik <sup3r44ax@yandex.ru>
2022-02-14 17:39:20 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+
+bot.polling(none_stop=True, interval=0)
